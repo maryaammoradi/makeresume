4124e3708935a8200a237a17c0dd32d3da461db7
maryam moradi <moradi.maryam7778@gmail.com>
2020-10-28 22:33:03 +0330

    Updates

---
+++ precache-manifest.f448c65f23dfdd2f35166d93fcd29114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3deb89bf97a889addea7a739b1df3757",
+    "url": "/makeresume/index.html"
+  },
+  {
+    "revision": "ef51263b6f464541c510",
+    "url": "/makeresume/static/css/main.5193acb7.chunk.css"
+  },
+  {
+    "revision": "17a9663a74e303dfe6b5",
+    "url": "/makeresume/static/js/2.51681744.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/makeresume/static/js/2.51681744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef51263b6f464541c510",
+    "url": "/makeresume/static/js/main.ce01bf9f.chunk.js"
+  },
+  {
+    "revision": "ba5b190d19b3eefac0bd",
+    "url": "/makeresume/static/js/runtime-main.638260c9.js"
+  }
+]);
